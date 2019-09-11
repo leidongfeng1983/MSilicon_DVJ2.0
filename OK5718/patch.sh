22c4161bb8bdc22ba4674b3bf5a03475194ef4ce
leidongfeng1983 <llldddfffldf@163.com>
2019-09-11 09:20:42 +0800

    fix hard code problem

--- OK5718/patch.sh
+++ OK5718/patch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd ..
-cp -rf /usr/share/djv2.0/sdk/OK5718-SDK-V1.0 ./
-cd MSilicon_DVJ2.0
+cp -rf /usr/share/djv2.0/sdk/OK5718-SDK-V1.0 ../
 cp -rf ./src/*  ../OK5718-SDK-V1.0/
 cp -rf ./patches/gpmc/OK57xx-linux-kernel/*  ../OK5718-SDK-V1.0/OK57xx-linux-kernel/
 cp -rf ./patches/hid/OK57xx-linux-kernel/*  ../OK5718-SDK-V1.0/OK57xx-linux-kernel/
--- debian/patch.sh
+++ debian/patch.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd ..
-cp -rf /usr/share/djv2.0/sdk/OK5718-SDK-V1.0 ./
-cd MSilicon_DVJ2.0
+cp -rf /usr/share/djv2.0/sdk/OK5718-SDK-V1.0 ../
 rm -rf ../OK5718-SDK-V1.0/OK57xx-linux-kernel/*
 cp -rf /usr/share/djv2.0/sdk/linux-4.14.79/*  ../OK5718-SDK-V1.0/OK57xx-linux-kernel/
 cp -rf ./src/*  ../OK5718-SDK-V1.0/
